7e5d2e147d0979a5f5832d3f95c070e9a349d2a3
gonzalo <gonzalo@ports.init.de>
2018-04-11 09:51:42 +0200

    fix for clang6

--- src/apachetop.cc
+++ src/apachetop.cc
@@ -271,7 +271,7 @@ int main(int argc, char *argv[])
 	hm->create(cf.circle_size);
 	/* }}} */
 
-	memset(&gstats, (char) NULL, sizeof(gstats));
+	memset(&gstats, 0, sizeof(gstats));
 	gstats.start = time(NULL);
 
 	signal(SIGINT, &catchsig);
@@ -465,7 +465,7 @@ int main(int argc, char *argv[])
 					continue;
 				}
 
-				*nextline = (char) NULL;
+				*nextline = '\0';
 				++nextline;
 
 				/* which parser? */
